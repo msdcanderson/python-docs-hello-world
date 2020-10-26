f7344b06500d8672651d9a21ae3ce8f063a6250a
Mike Anderson <michael.anderson@banksearch-consultancy.com>
2020-10-26 15:29:47 +0100

    modify app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World! This is a test"
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        return "Hello, World! This is the next test"
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    # app.run(port=5000, debug=True)
+    app.run()
